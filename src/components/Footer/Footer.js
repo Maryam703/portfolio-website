b9ccc4907fe811cf06a8d52350c9f5a74bae5c53
maryam <maryam.rizwan10@icloud.com>
2024-12-18 11:34:41 +0500

    fix a bug

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,9 +9,9 @@ function Footer() {
       © 2024 Maryam Mughal. All Right Reserved.
       </div>
       <div>
-        <Link className='footer-links' to="/linkedin" ><i className="fa-brands fa-linkedin"></i></Link>
+        <Link className='footer-links' to="https://www.linkedin.com/in/maryam-rizwan-a53423341?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" ><i className="fa-brands fa-linkedin"></i></Link>
         <Link className='footer-links' to="https://github.com/Maryam703"><i className="fa-brands fa-github"></i></Link>
-        <Link className='footer-links' to="/maryam.rizwan6@icloud.com"><i className="fa-solid fa-envelope"></i></Link>
+        <Link className='footer-links' to="https://icloud.com/maryam.rizwan6@icloud.com"><i className="fa-solid fa-envelope"></i></Link>
       </div>
     </div>
   )
