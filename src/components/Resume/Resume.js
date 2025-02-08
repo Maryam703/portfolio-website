285124a64d1583ad57959632dfba74fbeef4bd0d
maryam <maryam.rizwan10@icloud.com>
2025-02-08 23:11:57 +0500

    fix resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,7 +5,7 @@ function Resume() {
   return (
     <div className='resume-container'> 
       <div className='resume-img-container'>
-        <embed src='/images/resume.pdf' type="application/pdf" />
+        <embed src='/images/maryam.rizwan.pdf' type="application/pdf" />
       </div>
     </div>
   )
